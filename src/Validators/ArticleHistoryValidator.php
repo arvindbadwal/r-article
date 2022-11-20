88f6a586e9d86237bf70276f14be67c8731505b6
Arvind Badwal <arvind.badwal@cactusglobal.com>
2022-11-20 19:30:42 +0530

    remove unwanted

--- src/Validators/ArticleHistoryValidator.php
+++ src/Validators/ArticleHistoryValidator.php
@@ -5,7 +5,6 @@ namespace Cactus\Article\Validators;
 class ArticleHistoryValidator extends BaseValidator
 {
     public const ARTICLE_MARK_READ = "mark_read";
-    public const GET_HISTORY = "get_history";
 
     public function __construct()
     {
@@ -15,17 +14,10 @@ class ArticleHistoryValidator extends BaseValidator
                 'article_id' => 'required',
                 'read_via' => 'nullable|string'
             ],
-            self::GET_HISTORY => [
-                'count' => 'nullable|integer',
-                'offset' => 'nullable|integer',
-                'sort_by' => 'nullable|string|in:created_at',
-                'order_by' => 'nullable|string|in:asc,desc'
-            ],
         ];
 
         $this->messages = [
             self::ARTICLE_MARK_READ => [],
-            self::GET_HISTORY => [],
         ];
     }
 }
